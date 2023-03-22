7b87b6fa5e4cd9cd240e8c077d9058841cc74271
unknown <malexandresantos@hotmail.com>
2023-03-21 22:24:58 -0300

    .

---
+++ ExerciciosCOLEÇÕES/src/Exercícios/Exercício2.java
@@ -0,0 +1,47 @@
+package Exercícios;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Exercício2 {
+
+	public static void main(String[] args) {
+		
+		List<Integer> numeros = new ArrayList();
+		
+		Scanner leia = new Scanner(System.in);
+		
+		
+		
+		numeros.add(2);
+		numeros.add(5);
+		numeros.add(1);
+		numeros.add(3);
+		numeros.add(4);
+		numeros.add(9);
+		numeros.add(7);
+		numeros.add(8);
+		numeros.add(10);
+		numeros.add(6);
+		
+		System.out.println("----------------------------------------------");
+		System.out.println("Digite o número que deseja buscar no Array: ");
+		Integer verifica = leia.nextInt();
+		
+		if(numeros.contains(verifica)) {
+			System.out.println("------------------------------------------------------");
+			System.out.println("O numero existe dentro do Array e está na posição: "+numeros.indexOf(verifica));
+			System.out.println("------------------------------------------------------");
+		}else {
+			System.out.println("----------------------------------------------");
+			System.out.println("O numero não foi encontrado dentro do Array!");
+			System.out.println("----------------------------------------------");
+		}
+		
+		
+		
+
+	}
+
+}
---
+++ ExerciciosCOLEÇÕES/src/Exercícios/Exercício3.java
@@ -0,0 +1,38 @@
+package Exercícios;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Exercício3 {
+
+	public static void main(String[] args) {
+
+		Set<Integer> numeros = new HashSet();
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("----------------------------------------------");
+		System.out.println("Digite os numeros: ");
+		
+		for(int x=0;x<10;x++) {
+			int num = leia.nextInt(); 
+			numeros.add(num);
+		}
+		
+		
+		Iterator<Integer> numeros2 = numeros.iterator();
+		
+		System.out.println("----------------------------------------------");
+		System.out.println("Os dados listados foram: ");
+		System.out.println();
+		while(numeros2.hasNext()) {
+			System.out.println(numeros2.next());
+		}
+		
+		
+	}
+
+}
